ad414e9cbd63e62161caec82b78cb816caac9e85
Haigos <tim.zurbuchen@zebbra.ch>
2022-09-13 16:03:21 +0200

    Orders can be purchased from cart now. Order form added to gather shipping details. Added basic check for form inupt. Used carbon Inputs to build form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { ButtonModule } from "carbon-components-angular";
 import { CartComponent } from "./cart/cart.component";
 import { HeaderComponent } from "./header/header.component";
 import { HttpClientModule } from "@angular/common/http";
+import { InputModule } from "carbon-components-angular";
 import { ItemListComponent } from "./item-list/item-list.component";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { StructuredListModule } from "carbon-components-angular";
 import { TilesModule } from "carbon-components-angular";
@@ -18,6 +20,8 @@ import { TilesModule } from "carbon-components-angular";
     StructuredListModule,
     HttpClientModule,
     TilesModule,
+    ReactiveFormsModule,
+    InputModule,
     RouterModule.forRoot([
       { path: "", component: ItemListComponent },
       { path: "cart", component: CartComponent },
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { CartService } from "../cart.service";
 import { Component } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 import { Product } from "../products";
 
 @Component({
@@ -8,9 +9,27 @@ import { Product } from "../products";
   styleUrls: ["./cart.component.scss"],
 })
 export class CartComponent {
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) {}
+
+  checkoutForm = this.formBuilder.group({
+    name: "",
+    address: "",
+  });
 
   products = this.cartService.getItems();
+
+  onSubmit() {
+    if (this.checkoutForm.value.name === "" || this.checkoutForm.value.address === "") {
+      window.alert("Please fill out fields.");
+    } else {
+      this.products = this.cartService.clearCart();
+      window.alert("Check the console for details concerning your order.");
+      console.warn(
+        "Order for '" + this.checkoutForm.value.name + "' will be shipped to: " + this.checkoutForm.value.address
+      );
+      this.checkoutForm.reset();
+    }
+  }
 
   deleteProduct(product: Product) {
     this.cartService.deleteProductFromCart(product);
