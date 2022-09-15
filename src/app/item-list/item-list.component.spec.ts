382a6c020439c591af6d8657a4a76fdb77a53bc8
Haigos <tim.zurbuchen@zebbra.ch>
2022-09-15 09:44:20 +0200

    drone test 2342340923509 :')

--- src/app/item-list/item-list.component.spec.ts
+++ src/app/item-list/item-list.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ItemListComponent } from "./item-list.component";
 
 describe("ItemListComponent", () => {
@@ -8,6 +8,7 @@ describe("ItemListComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ItemListComponent],
     }).compileComponents();
 
